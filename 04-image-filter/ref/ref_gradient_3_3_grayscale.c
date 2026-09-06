30567fad39e66050c7f96a56935cc05df3b66cef
kotovro <kotvoro@gmail.com>
2026-09-06 14:38:04 +0300

    test: Adds reference filtered images so now it's possible to test filter in acceptance-tests

---
+++ 04-image-filter/ref/ref_gradient_3_3_grayscale.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("P3\n3 3\n255\n");
+	printf("150 150 150 188 188 188 226 226 226\n");
+	printf(" 75  75  75 113 113 113 151 151 151\n");
+	printf("  0   0   0  38  38  38  76  76  76\n");
+	return 0;
+}            
+ 
+  
